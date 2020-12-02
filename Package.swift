e4b5ce5a10753984350ef0da044193343a2dbc03
Giselle Takahashi <giselle.takahashi@modmed.com>
2020-12-02 10:50:50 -0500

    adjustments for header

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "jsonmodel",
+    name: "JSONModel",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "jsonmodel",
-            targets: ["jsonmodel"]),
+            name: "JSONModel",
+            targets: ["JSONModel"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "jsonmodel",
+            name: "JSONModel",
             dependencies: [],
             path: "JSONModel",
-            exclude: ["Info.plist"]
+            exclude: ["Info.plist"],
+            publicHeadersPath: ""
             ),
     ]
 )
