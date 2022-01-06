7ccd2bba79b1c530a9773cf921b2200e83371168
Ricardo S Alba <risalba@gmail.com>
2022-01-06 18:25:17 +0100

    Make dynamic a new target

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "JSONModel",
+            targets: ["JSONModel"]),
+        .library(
+            name: "JSONModelDynamic",
             type: .dynamic,
             targets: ["JSONModel"]),
     ],
